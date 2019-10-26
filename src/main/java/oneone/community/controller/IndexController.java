bd8fe0cf3c64bc8107db71ce8e80b842383e69b3
GitVide <1162065497@qq.com>
2019-10-27 00:06:38 +0800

    add flyway migration support

--- src/main/java/oneone/community/controller/IndexController.java
+++ src/main/java/oneone/community/controller/IndexController.java
@@ -1,12 +1,38 @@
 package oneone.community.controller;
 
+import oneone.community.mapper.UserMapper;
+import oneone.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        System.out.println("-------------");
+        Cookie[] cookies = request.getCookies();
+        System.out.println(cookies);
+        if (cookies != null){
+            for(Cookie cookie : cookies){
+                if (cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findUserByToken(token);
+                    if (user != null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+        }
+
         return "index";
     }
 }
